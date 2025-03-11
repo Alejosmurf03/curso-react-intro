9ae7758239e985805127ffec920d5d98d51534ec
Alejosmurf03 <vegariverosmanuelalejandro@gmail.com>
2025-03-11 11:11:34 -0500

    Se añaden componentes de error carga o vacios

--- src/App/index.js
+++ src/App/index.js
@@ -13,7 +13,12 @@ import { useLocalStorage } from './useLocalStorage';
 // localStorage.removeItem('TODOS_V1');
 
 function App() {
-    const [todos, saveTodos] = useLocalStorage('TODOS_V1', []);
+    const {
+        item: todos,
+        saveItem: saveTodos,
+        loading,
+        error,
+    } = useLocalStorage('TODOS_V1', []);
     const [searchValue, setSearchValue] = React.useState('');
 
     const completedTodos = todos.filter(
@@ -49,6 +54,8 @@ function App() {
 
     return (
         <AppUI
+            loading={loading}
+            error={error}
             completedTodos = {completedTodos}
             totalTodos = {totalTodos}
             searchValue = {searchValue}
